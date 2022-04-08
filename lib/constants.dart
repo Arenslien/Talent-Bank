2a654ef0576a43ebed039dd544f3c4de6d834b91
Arenslien <arenslien@gmail.com>
2022-04-08 09:39:36 +0900

    constant 파일 수정

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kRedColor = Color(0xFFFF6F6F);
+const kYellowColor = Color(0xFFFFF46E);
+const kBackGroundColor = Color(0xFFF6F6F6);
+const kPurpleColor = Color(0xFFA58BFF);
+const kTextColor = Color(0xFF757575);
